dd32b0b84ce95a8207407fe2d1b2a09ece19da1c
dm-podporin <dm.podporin@gmail.com>
2023-07-02 14:38:29 +0300

    solution recreation

---
+++ Accessor/Controllers/PhoneBookController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Manager.Models;
+using Accessor.Services;
+
+namespace Manager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhoneBookController : ControllerBase
+    {
+        private readonly PhoneBookServices _phoneBookServices;
+
+        public PhoneBookController(
+            PhoneBookServices phoneBookServices)
+        {
+            _phoneBookServices = phoneBookServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Contact>>> GetContacts()
+        {
+            try
+            {
+
+                var result = await _phoneBookServices.GetContact();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ContactDTO>> CreateContact(ContactDTO contact)
+        {
+            try
+            {
+
+                var result = await _phoneBookServices.CreateContact(contact);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Accessor/Services/PhoneBookServices.cs
@@ -0,0 +1,45 @@
+﻿using Manager.Models;
+using MongoDB.Driver;
+
+namespace Accessor.Services
+{
+    public class PhoneBookServices
+    {
+        private readonly MongoDbContext _mongoClient;
+
+        public PhoneBookServices(MongoDbContext mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        public async Task<List<Contact>?> GetContact()
+        {
+            try
+            {
+                var result = await _mongoClient.PhoneBook.Find(contact => true).ToListAsync();
+
+                return result;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<ContactDTO> CreateContact(ContactDTO contact)
+        {
+            try
+            {
+
+                await _mongoClient.PhoneBookPost.InsertOneAsync(contact);
+
+                return contact;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
